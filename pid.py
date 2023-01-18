59cb1c28895a73883b0a90609039cbdc096c31d0
V0id-Karanveer <117469425+V0id-Karanveer@users.noreply.github.com>
2023-01-18 20:37:39 +0530

    Create pid.py

---
+++ pid.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import rospy
+from simple_pid import PID
+from std_msgs.msg import Float64, Float64MultiArray
+from dynamic_reconfigure.server import Server
+
+class Node:
+    input = 0
+    setpoint = 0
+
+    def __init__(self):
+        p = rospy.get_param('~p', 1)
+        i = rospy.get_param('~i', 0.1)
+        d = rospy.get_param('~d', 0.05)
+        input_topic = rospy.get_param('~input') 
+        adjusted_topic = rospy.get_param('~adjusted')
+        setpoint_topic = rospy.get_param('~setpoint')
+        configuration_topic = rospy.get_param('~configuration')
+        publish_rate = rospy.get_param('~publish_rate', 100)
+
+        rospy.Subscriber(input_topic, Float64, self.inputCallback, queue_size=1)
+        rospy.Subscriber(setpoint_topic, Float64, self.setpointCallback, queue_size=1)
+        rospy.Subscriber(configuration_topic, Float64MultiArray, self.configurationCallback, queue_size=1)
+        self.publisher = rospy.Publisher(adjusted_topic, Float64, queue_size=1)
+        
+        self.message = Float64()
+        self.rate = rospy.Rate(publish_rate)
+
+        self.pid = PID(p, i, d)
+        
+    def inputCallback(self, msg):
+        self.input = msg.data
+
+    def configurationCallback(self, msg):
+        self.pid.Kp = msg.data[0]
+        self.pid.Ki = msg.data[1]
+        self.pid.Kd = msg.data[2]
+
+    def setpointCallback(self, msg):
+        self.setpoint = msg.data
+
+    def reconfigureCallback(config, level):
+        self.pid.Kp = config["p"]
+        self.pid.Ki = config["i"]
+        self.pid.Kd = config["d"]
+
+        return config
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.pid.setpoint = self.setpoint
+            self.message.data = pid(self.input)
+            self.publisher.publish(self.message)
+            
+            self.rate.sleep()
+
+def main():
+    rospy.init_node('rugved_karanveer', anonymous=True)
+
+    node = Node()
+    node.run()
+
+if __name__ == '__main__':
+    main()
